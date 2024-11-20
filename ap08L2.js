ef20a3edb583d2b910a25f86bf43301293d21d3a
G384692023 <134909051+G384692023@users.noreply.github.com>
2024-11-20 14:50:09 +0900

    課題終了

--- ap08L2.js
+++ ap08L2.js
@@ -53,7 +53,22 @@ export function init(scene, size, id, offset, texture) {
         const height = [2, 2, 7, 4, 5];
         const bldgH = height [type]*5;
         const geometry = new THREE. BoxGeometry (8, bldgH, 8);
-        const material = new THREE.MeshLambertMaterial({color: 0x808080}); 
+        const material = new THREE.MeshLambertMaterial({map:texture});
+        const sideUvS = (type*2+1)/11; 
+        const sideUvE = (type*2+2)/11;
+        const topUvS = (type*2+2)/11;
+        const topUVE = (type*2+3)/11;
+        const uvs = geometry.getAttribute("uv");
+        for (let i = 0; i < 48; i+=4) {
+            if (i<16 || i > 22) {
+                uvs.array[i]= sideUvS;
+                uvs.array[i+2]= sideUvE;
+            }
+            else {
+                uvs.array[i]=topUvS;
+                uvs.array[i+2] = topUVE;
+            }
+        }
         const bldg = new THREE.Mesh(
         geometry,
         material
